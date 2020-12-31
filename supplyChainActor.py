e5e3468ac6f31f0a49fe4cd17f9ce1fdd23c2546
ulku <ulku.bayraktar@beyaz.net>
2020-12-31 10:27:45 +0300

    supplyChainActor class completed (so i hope that)

--- supplyChainActor.py
+++ supplyChainActor.py
@@ -20,6 +20,76 @@ class SupplyChainActor:
 
         return
 
+
+
+    def PlaceOutgoingDelivery(self, amountToDeliver):
+        self.outgoingDeliveriesQueue.PushEnvelope(amountToDeliver)
+        return
+
+
+    def PlaceOutgoingOrder(self, weekNum):
+        # Calculates the order quantity using an anchor and maintain
+        # strategy.
+
+        #First weeks are in equilibrium
+        if weekNum <= 4:
+            amountToOrder = 4
+        #After first few weeks, the actor chooses the order. We use "anchor and maintain" strategy.
+        else:
+            #We want to cover any out flows, we know that there are some orders in the pipeline.
+            amountToOrder = 0.5 * self.currentOrders
+
+            if (TARGET_STOCK - self.currentStock) > 0:
+                amountToOrder += TARGET_STOCK - self.currentStock
+
+        self.outgoingOrdersQueue.PushEnvelope(amountToOrder)
+        self.lastOrderQuantity = amountToOrder
+
+        return
+
+
+    def ReceiveIncomingDelivery(self):
+        # Updates the current stock based on the incoming
+        # deliveries queue.
+        quantityReceived = self.incomingDeliveriesQueue.PopEnvelope()
+
+        if quantityReceived > 0:
+            self.currentStock += quantityReceived
+
+        return
+
+
+    def ReceiveIncomingOrders(self):
+        # Updates the current orders based on the incoming
+        # deliveries queue.
+        thisOrder = self.incomingOrdersQueue.PopEnvelope()
+
+        if thisOrder > 0:
+            self.currentOrders += thisOrder
+        return
+
+
+    def CalcBeerToDeliver(self):
+        # Returns deliveryQuantitiy - the number of cases to be delivered
+
+        # to the customer. currentOrders, currentStock are
+        # updated to reflect this delivery quantity.
+
+        deliveryQuantity = 0
+         #If we can fill the customer's order, we must do it.
+        if self.currentStock >= self.currentOrders:
+            deliveryQuantity = self.currentOrders
+            self.currentStock -= deliveryQuantity
+            self.currentOrders -= deliveryQuantity
+        #If the current stock cannot cover the order, we must fill as much as we can, and back-order the rest.
+        elif self.currentStock >= 0 and self.currentStock < self.currentOrders:
+            deliveryQuantity = self.currentStock
+            self.currentStock = 0
+            self.currentOrders -= deliveryQuantity
+
+        return deliveryQuantity
+
+
     def CalcCostForTurn(self):
         # Returns costsThisTurn - the total cost incurred during
 
