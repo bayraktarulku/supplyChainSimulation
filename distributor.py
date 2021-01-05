c8b58928e638402a88b9940f15b40ad530e26881
ulku <ulku.bayraktar@beyaz.net>
2021-01-05 10:10:19 +0300

    distributor class completed. SupplyChainStatistics created (WIP)

--- distributor.py
+++ distributor.py
@@ -1,3 +1,38 @@
 # -------------------------------------------------------
 # This file contains and defines the Distributor class.
 # -------------------------------------------------------
+
+from supplyChainActor import SupplyChainActor
+
+class Distributor(SupplyChainActor):
+
+    def __init__(self, incomingOrdersQueue, outgoingOrdersQueue, incomingDeliveriesQueue, outgoingDeliveriesQueue):
+        # Initializes the Distributor object in its initial state
+        # by calling parent constructor.
+        super().__init__(incomingOrdersQueue, outgoingOrdersQueue,
+                         incomingDeliveriesQueue, outgoingDeliveriesQueue)
+        return
+
+
+    def TakeTurn(self, weekNum):
+        #The steps for taking a turn are as follows:
+
+        #RECEIVE NEW DELIVERY FROM FACTORY
+        self.ReceiveIncomingDelivery()    #This also advances the queue!
+
+        #RECEIVE NEW ORDER FROM WHOLESALER
+        self.ReceiveIncomingOrders()     #This also advances the queue!
+
+        #PREPARE DELIVERY
+        if weekNum <= 4:
+            self.PlaceOutgoingDelivery(4)
+        else:
+            self.PlaceOutgoingDelivery(self.CalcBeerToDeliver())
+
+        #PLACE ORDER
+        self.PlaceOutgoingOrder(weekNum)
+
+        #UPDATE COSTS
+        self.costsIncurred += self.CalcCostForTurn()
+
+        return
--- supplyChainStatistics.py
+++ supplyChainStatistics.py
@@ -1,6 +1,69 @@
 # This file contains and defines the SupplyChainStatistics class.
 
+import matplotlib.pyplot as plt
+
 class SupplyChainStatistics:
 
     def __init__(self):
-        pass
+
+        #Cost statistics
+        self.retailerCostsOverTime = []
+        self.wholesalerCostsOverTime = []
+        self.distributorCostsOverTime = []
+        self.factoryCostsOverTime = []
+
+        #Order statistics
+        self.retailerOrdersOverTime = []
+        self.wholesalerOrdersOverTime = []
+        self.distributorOrdersOverTime = []
+        self.factoryOrdersOverTime = []
+
+        #Effective inventory statistics
+        self.retailerEffectiveInventoryOverTime = []
+        self.wholesalerEffectiveInventoryOverTime = []
+        self.distributorEffectiveInventoryOverTime = []
+        self.factoryEffectiveInventoryOverTime = []
+
+        return
+
+
+    def RecordRetailerOrders(self, retailerOrdersThisWeek):
+        # retailerOrdersThisWeek is appended to
+        # retailerOrdersOverTime, a list which tracks the retailer's
+        # weekly orders.
+        self.retailerOrdersOverTime.append(retailerOrdersThisWeek)
+        return
+
+
+    def RecordWholesalerOrders(self, wholesalerOrdersThisWeek):
+        self.wholesalerOrdersOverTime.append(wholesalerOrdersThisWeek)
+        return
+
+
+    def RecordDistributorOrders(self, distributorOrdersThisWeek):
+
+        self.distributorOrdersOverTime.append(distributorOrdersThisWeek)
+        return
+
+
+    def RecordFactoryOrders(self, factoryOrdersThisWeek):
+        self.factoryOrdersOverTime.append(factoryOrdersThisWeek)
+        return
+
+
+    def RecordRetailerCost(self, retailerCostsThisWeek):
+        self.retailerCostsOverTime.append(retailerCostsThisWeek)
+        return
+
+
+    def RecordWholesalerCost(self, wholesalerCostsThisWeek):
+        self.wholesalerCostsOverTime.append(wholesalerCostsThisWeek)
+        return
+
+
+    def RecordDistributorCost(self, distributorCostsThisWeek):
+        self.distributorCostsOverTime.append(distributorCostsThisWeek)
+        return
+
+    # def RecordFactoryCost(self, factoryCostsThisWeek):
+        # pass
